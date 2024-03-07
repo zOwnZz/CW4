743057fed65c9655f9e92b9cfcf4edc6fc112536
BGlowacki-kcl <152788864+BGlowacki-kcl@users.noreply.github.com>
2024-03-07 12:05:55 +0000

    Update BaseGUI.java

--- BaseGUI.java
+++ BaseGUI.java
@@ -1,11 +1,35 @@
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 public abstract class BaseGUI {
+    protected int WIN_WIDTH = 1000;
+    protected int WIN_HEIGHT = 600;
 
+    /**
+     * Abstract method that returns a scene. Needs to be implemented in each GUI class
+     */
     public abstract Scene getScene();
-    private VBox menuTab(){
-        return new VBox();
-    }
 
+    /**
+     * The basic scene containing menu tab and buttons at the bottom
+     */
+    protected BorderPane getRoot(){
+        BorderPane root = new BorderPane();
+
+        root.setBottom(buttons());
+        root.setTop(menuTab());
+
+        return root;
+    }
+    private VBox menuTab(){
+        VBox menuTab = new VBox();
+        // Code for menu tab
+        return menuTab;
+    }
+    private VBox buttons() {
+        VBox buttons = new VBox();
+        // Code for buttons
+        return buttons;
+    }
 }
