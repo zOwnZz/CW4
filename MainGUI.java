867d50a54c3b42b767547b4717ce82473cd788c7
BGlowacki-kcl <152788864+BGlowacki-kcl@users.noreply.github.com>
2024-03-07 09:40:42 +0000

    Update MainGUI.java

--- MainGUI.java
+++ MainGUI.java
@@ -4,21 +4,25 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class MainGUI extends Application {
+    private int WIN_WIDTH = 1000;
+    private int WIN_HEIGHT = 600;
 
     @Override
     public void start(Stage primaryStage) {
+        Controller controller = new Controller();
+        ChallengeGUI chellange = new ChallengeGUI(WIN_WIDTH, WIN_HEIGHT, controller);
         // Create a root node.
         StackPane root = new StackPane();
-        
+
         // Create a scene with the root node with dimensions 300x250 (width x height)
-        Scene scene = new Scene(root, 500, 350);
-        
+        Scene scene = chellange.getScene();
+
         // Set the scene on the stage
         primaryStage.setScene(scene);
-        
+
         // Set the title of the stage (window)
         primaryStage.setTitle("COVID DATA");
-        
+
         // Show the stage
         primaryStage.show();
     }
