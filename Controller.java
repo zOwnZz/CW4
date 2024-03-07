533ed5c39b3e638da7dcedf94d71ac6d97225796
BGlowacki-kcl <152788864+BGlowacki-kcl@users.noreply.github.com>
2024-03-07 20:17:02 +0000

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -8,4 +8,16 @@ public class Controller {
         data = loader.load();
     }
     // method returning specific data for scenes
+    public HashMap<String, ArrayList<CovidData>> boroughAndData() {
+        HashMap<String, ArrayList<CovidData>> boroughAndData = new HashMap<>();
+        for(CovidData covid : data){
+            if(boroughAndData.containsKey(covid.getBorough()))
+                boroughAndData.get(covid.getBorough()).add(covid);
+            else {
+                boroughAndData.put(covid.getBorough(), new ArrayList<CovidData>());
+                boroughAndData.get(covid.getBorough()).add(covid);
+            }
+        }
+        return boroughAndData;
+    }
 }
