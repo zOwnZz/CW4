178f9ffc3f395bc35144d527f1a53d791f339a65
BGlowacki-kcl <152788864+BGlowacki-kcl@users.noreply.github.com>
2024-03-07 09:40:18 +0000

    Update ChallengeGUI.java

--- ChallengeGUI.java
+++ ChallengeGUI.java
@@ -1,1 +1,17 @@
-// Hello
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+
+public class ChallengeGUI extends BaseGUI {
+    private int WIN_WIDTH, WIN_HEIGHT;
+    private Controller controller;
+
+    public ChallengeGUI(int width, int height, Controller controller){
+        WIN_WIDTH = width;
+        WIN_HEIGHT = height;
+        this.controller = controller;
+    }
+    public Scene getScene(){
+        VBox root = new VBox();
+        return new Scene(root, WIN_WIDTH, WIN_HEIGHT);
+    }
+}
