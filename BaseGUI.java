9710b50e5054188be5a6b312105aa371c1cfead0
A1SHARIF <116805205+A1SHARIF@users.noreply.github.com>
2024-03-12 15:42:18 +0000

    Update BaseGUI.java

--- BaseGUI.java
+++ BaseGUI.java
@@ -8,26 +8,39 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.DateCell;
+import java.util.ArrayList;
 
-public abstract class BaseGUI {
+public class BaseGUI {
     private DatePicker startDatePicker, endDatePicker;
     
     protected int WIN_WIDTH = 1000;
     protected int WIN_HEIGHT = 600;
     private LocalDate minDate, maxDate, startDate, endDate;
+    public ArrayList<BaseGUI> pages = new ArrayList<>();
+    public int currentPageIndex = 0;
+    private Scene currentScene;
+    private Controller controller;
+    private HBox buttons;
+    public BaseGUI(Controller controller, HBox buttons){
+        this.controller = controller;
+        this.buttons = buttons;
+    }
     /**
      * Abstract method that returns a scene. Needs to be implemented in each GUI class
      */
-    public abstract Scene getScene();
-
+    public Scene getScene(){
+        return new Scene(new VBox(), WIN_WIDTH, WIN_HEIGHT);
+    };
+    
     /**
      * The basic scene containing menu tab and buttons at the bottom
      */
     protected BorderPane getRoot(){
         BorderPane root = new BorderPane();
 
-        root.setBottom(buttons());
+        root.setBottom(buttons);
         root.setTop(menuTab());
+        
    
         return root;
     }
@@ -67,38 +80,14 @@ public abstract class BaseGUI {
         
         return menuTab;
     }
-    private HBox buttons() {
-        HBox buttons = new HBox();
-        // Code for buttons
-        Button backButton = new Button(">");
-        Button nextButton = new Button("<");
-        buttons.setSpacing(945);
-        
-        nextButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                // Define the action to be performed when next button is clicked
-                //handleDateSelection();
-                System.out.println("Next Button clicked");
-            }
-        });
-
-        backButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                // Define the action to be performed when back button is clicked
-                //handleDateSelection();
-                System.out.println("Back Button clicked");
-            }
-        });
-        
-        buttons.getChildren().addAll(nextButton, backButton);
-        return buttons;
-        
-        
-    }
     
-    /*private boolean validateDates(LocalDate startDate, LocalDate endDate) {
+    
+    
+     
+
+
+    
+    private boolean validateDates(LocalDate startDate, LocalDate endDate) {
         if (startDate == null || endDate == null) {
             return false;
         }
@@ -116,12 +105,10 @@ public abstract class BaseGUI {
         // DateSelected=true;
         startDate = startDatePicker.getValue();
         endDate = endDatePicker.getValue();
-        //Alert alert = new Alert(AlertType.ERROR);
+        
 
         if (!validateDates(startDate, endDate)) {
-            //alert.setTitle("Invalid Dates Selected");
-            //alert.setHeaderText("Please select a valid date range.");
-            //alert.showAndWait();
+            
             System.out.println("wrong dates chosen");
 
             startDatePicker.setValue(null);
@@ -129,5 +116,5 @@ public abstract class BaseGUI {
             return;
         }
     
-    }*/  
+    }  
 }
