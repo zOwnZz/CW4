dd9e941725fe002299d477020ec98b2370b57bb6
A1SHARIF <116805205+A1SHARIF@users.noreply.github.com>
2024-03-12 15:47:09 +0000

    Update WelcomeGUI.java

--- WelcomeGUI.java
+++ WelcomeGUI.java
@@ -1,1 +1,40 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Pos;
+public class WelcomeGUI extends BaseGUI {
+     private Controller controller;
+    public WelcomeGUI(Controller controller, HBox buttons){
+        super(controller, buttons);
+        this.controller = controller;
+        
+    }
+    @Override
+    public Scene getScene() {
+        BorderPane root = getRoot();
+        
+        VBox center = new VBox();
+    
+        Label InfoLabel = new Label("This application visualises the impact of Covid-19 in London.");
+        
+        // Create a layout to stack the labels on top of each other
+        StackPane stackPane = new StackPane();
+        // Add the labels to the layout
+        stackPane.getChildren().addAll( InfoLabel);
 
+        // Create a scene with the layout
+        Scene scene = new Scene(stackPane, 300, 200);
+        center.getChildren().addAll(InfoLabel);
+        center.setAlignment(Pos.CENTER);
+        center.setSpacing(30);
+        //return new Scene(root, WIN_WIDTH, WIN_HEIGHT);
+        //return scene;
+        root.setCenter(center);
+        return new Scene(root, WIN_WIDTH, WIN_HEIGHT);
+    }
+}
