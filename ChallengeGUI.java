8b5c903baafb0653fd225bfb1cf55c563bd64810
BGlowacki-kcl <152788864+BGlowacki-kcl@users.noreply.github.com>
2024-03-12 23:10:13 +0000

    Update ChallengeGUI.java

--- ChallengeGUI.java
+++ ChallengeGUI.java
@@ -1,5 +1,6 @@
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -16,20 +17,29 @@ import java.util.HashMap;
 public class ChallengeGUI extends BaseGUI {
     private Controller controller;
     private ChallengeControl simulator;
-
-    /*
-    Create simulator with a change of the borough. As a parameter provide a borough's name. In the hash map provide
-    simulator with arrayList. Save this arrayList there. do the same functionality as in game of life to run the
-    simulation. Create slider to adjust velocity of the simulation. Reset progress bars when the borough is changed.
-    Disable changing borough when simulation is running. Stop simulation with the button.
-     */
-
-    public ChallengeGUI(Controller controller){
-        this.controller = controller;
-        this.simulator = new ChallengeControl(controller);
+    private Boolean firstChart;
+    private String cssForBars(String color){
+        return "-fx-background-color: #392467; " + // Background color
+               "-fx-accent: " + color + "; " + // Progress color
+               "-fx-text-box-border: #392467; " + // Border color
+               "-fx-control-inner-background: #FFFFEC; " + // Background color of the progress bar itself
+               "-fx-border-width: 1px; " + // Border width
+               "-fx-border-radius: 0px; " + // Border radius
+               "-fx-padding: 1px;"; // Padding
     }
+
+    public ChallengeGUI(Controller controller, ControllerGUI controllerGUI){
+        super(controller, controllerGUI);
+        this.controller = controller;
+    }
+
+
+
+    @Override
     public Scene getScene(){
         BorderPane root = getRoot();
+
+        firstChart = true;
 
         HashMap<String, ArrayList<CovidData>> boroughAndData = controller.boroughAndData();
 
@@ -43,6 +53,14 @@ public class ChallengeGUI extends BaseGUI {
         Button startSimulation = new Button("Play!");
         Button stopSimulation = new Button("Stop!");
 
+        startSimulation.setOnAction(event -> {
+            simulator.playSimulation();
+            startSimulation.setDisable(true);
+        });
+        stopSimulation.setOnAction(event -> {
+            simulator.stopSimulation();
+            startSimulation.setDisable(false);
+        });
 
         selection.getItems().addAll(boroughAndData.keySet());
 
@@ -66,36 +84,67 @@ public class ChallengeGUI extends BaseGUI {
 
         ArrayList<HBox> healthBarsPanes = new ArrayList<>(Arrays.asList(retAndRecPane, grocAndPhaPane, parksPane, transitPane, workplacePane, residentialPane));
 
-        ProgressBar retAndRecBar = new ProgressBar(); retAndRecPane.getChildren().add(retAndRecBar);
-        ProgressBar grocAndPhaBar = new ProgressBar(); grocAndPhaPane.getChildren().add(grocAndPhaBar);
-        ProgressBar parksBar = new ProgressBar(); parksPane.getChildren().add(parksBar);
-        ProgressBar transitBar = new ProgressBar(); transitPane.getChildren().add(transitBar);
-        ProgressBar workplaceBar = new ProgressBar(); workplacePane.getChildren().add(workplaceBar);
-        ProgressBar residentialBar = new ProgressBar(); residentialPane.getChildren().add(residentialBar);
+        Label retAndRecLabel = new Label("");
+        Label grocAndPhaLabel = new Label("");
+        Label parksLabel = new Label("");
+        Label transitLabel = new Label("");
+        Label workplaceLabel = new Label("");
+        Label residentialLabel = new Label("");
+
+        ArrayList<Label> labelsPopulation = new ArrayList<>(Arrays.asList(retAndRecLabel, grocAndPhaLabel, parksLabel, transitLabel, workplaceLabel, residentialLabel));
+
+        ProgressBar retAndRecBar = new ProgressBar(); retAndRecPane.getChildren().addAll(retAndRecBar, retAndRecLabel);
+        ProgressBar grocAndPhaBar = new ProgressBar(); grocAndPhaPane.getChildren().addAll(grocAndPhaBar, grocAndPhaLabel);
+        ProgressBar parksBar = new ProgressBar(); parksPane.getChildren().addAll(parksBar, parksLabel);
+        ProgressBar transitBar = new ProgressBar(); transitPane.getChildren().addAll(transitBar, transitLabel);
+        ProgressBar workplaceBar = new ProgressBar(); workplacePane.getChildren().addAll(workplaceBar, workplaceLabel);
+        ProgressBar residentialBar = new ProgressBar(); residentialPane.getChildren().addAll(residentialBar, residentialLabel);
+
+        retAndRecBar.setStyle(cssForBars("#9195F6"));
+        grocAndPhaBar.setStyle(cssForBars("#BFEA7C"));
+        parksBar.setStyle(cssForBars("#416D19"));
+        transitBar.setStyle(cssForBars("#9B4444"));
+        workplaceBar.setStyle(cssForBars("#FC6736"));
+        residentialBar.setStyle(cssForBars("#DC84F3"));
 
         ArrayList<ProgressBar> healthBars = new ArrayList<>(Arrays.asList(retAndRecBar, grocAndPhaBar, parksBar, transitBar, workplaceBar, residentialBar));
 
         for(HBox pane : healthBarsPanes){
             pane.setAlignment(Pos.CENTER);
             healthBarsPane.getChildren().addAll(pane);
+            pane.setSpacing(10);
         }
 
         for(ProgressBar bar : healthBars){
-            bar.setProgress(1);
-            bar.setPrefWidth(200);
+            bar.setProgress((double) 1 /6);
+            bar.setPrefWidth(600);
         }
 
         healthBarsPane.setSpacing(10);
         healthBarsPane.setAlignment(Pos.CENTER);
 
-        /*HashMap<String, ArrayList<CovidData>> boroughAndData = controller.boroughAndData();
-        for(CovidData covid : boroughAndData.get("Enfield")){
-            box.getChildren().add(new Label(covid.getDate()));
-        }*/
+        selection.setOnAction(event -> {
+            String selectedOption = selection.getSelectionModel().getSelectedItem();
+            for(ProgressBar bar : healthBars){
+                bar.setProgress((double) 1/6);
+                bar.setPrefWidth(600);
+            }
+            if(!firstChart) {
+                simulator.disableSimulation();
+                center.getChildren().remove(center.getChildren().size() - 1);
+            }
+            simulator = new ChallengeControl(controller, healthBars, selectedOption, labelsPopulation);
+            LineChart<Number, Number> lineChart =  simulator.createChart();
+            firstChart = false;
+            for(Label label : labelsPopulation)
+                label.setText(String.valueOf(simulator.getInitialNumber()));
+            center.getChildren().add(lineChart);
+            startSimulation.setDisable(false);
+        });
 
         center.getChildren().addAll(upperPane, healthBarsPane);
         center.setAlignment(Pos.CENTER);
-        center.setSpacing(30);
+        center.setSpacing(15);
 
         root.setCenter(center);
 
