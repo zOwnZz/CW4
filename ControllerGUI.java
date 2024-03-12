7fc724cc5133616f5866bb24aed04a8c3ca3a88b
Saad Abdeen <121445330+zOwnZz@users.noreply.github.com>
2024-03-12 14:37:48 +0000

    Update ControllerGUI.java

--- ControllerGUI.java
+++ ControllerGUI.java
@@ -1,30 +1,32 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class ControllerGUI extends Application {
 
+public class ControllerGUI extends Application {
+    
     @Override
     public void start(Stage primaryStage) {
         Controller controller = new Controller();
-        BaseGUI challenge = new ChallengeGUI(controller);
+        StatisticsGUI statisticsGUI = new StatisticsGUI(controller);
+        Scene scene = statisticsGUI.getScene();
 
-        // Get all scenes for the project
-        Scene scene = challenge.getScene();
-
-        // Set the scene on the stage
         primaryStage.setScene(scene);
-
-        // Set the title of the stage (window)
-        primaryStage.setTitle("COVID DATA");
-
-        // Show the stage
+        primaryStage.setTitle("COVID-19 Statistics Panel");
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-        // Launch the JavaFX application
         launch(args);
     }
 }
+
+
+
+
+
+
+
+
+
+  
