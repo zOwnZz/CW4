20ec340a7e64738bae7cc0d244f9da5f6a8119cd
BGlowacki-kcl <152788864+BGlowacki-kcl@users.noreply.github.com>
2024-03-12 23:11:44 +0000

    Update BaseGUI.java

--- BaseGUI.java
+++ BaseGUI.java
@@ -2,23 +2,35 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.*;
-import javafx.event.*;
 import java.time.LocalDate;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.DateCell;
 
-public abstract class BaseGUI {
-    private DatePicker startDatePicker, endDatePicker;
+public class BaseGUI {
+
 
     protected int WIN_WIDTH = 1000;
     protected int WIN_HEIGHT = 600;
+    private Controller controller;
     private LocalDate minDate, maxDate, startDate, endDate;
+    private DatePicker startDatePicker, endDatePicker;
+    private ControllerGUI controllerGUI;
+
+
+    public BaseGUI(Controller controller, ControllerGUI controllerGUI){
+        this.controller = controller;
+        this.controllerGUI = controllerGUI;
+        minDate = controller.getMinDate();
+        maxDate = controller.getMaxDate();
+    }
+
     /**
-     * Abstract method that returns a scene. Needs to be implemented in each GUI class
+     * Method that returns a scene. Should be overriden in each GUI class
      */
-    public abstract Scene getScene();
+    public Scene getScene(){
+        return new Scene(new VBox(), WIN_WIDTH, WIN_HEIGHT);
+    };
 
     /**
      * The basic scene containing menu tab and buttons at the bottom
@@ -28,28 +40,47 @@ public abstract class BaseGUI {
 
         root.setBottom(buttons());
         root.setTop(menuTab());
-        root.setPrefWidth(WIN_WIDTH);
 
         return root;
     }
+
+    private HBox buttons() {
+        HBox buttons = new HBox();
+
+        Button nextButton = new Button(">");
+        Button backButton = new Button("<");
+        // Make win width dynamic, to be resizable
+        buttons.setSpacing(WIN_WIDTH-50);
+
+        backButton.setOnAction(event -> controllerGUI.changeScene(false));
+
+        nextButton.setOnAction(event -> controllerGUI.changeScene(true));
+
+        if(!controllerGUI.ifAvailable){
+            backButton.setDisable(true);
+            nextButton.setDisable(true);
+        } else {
+            backButton.setDisable(false);
+            nextButton.setDisable(false);
+        }
+
+        buttons.getChildren().addAll(backButton, nextButton);
+        return buttons;
+    }
+
     private HBox menuTab(){
         HBox menuTab = new HBox();
-        // Code for menu tab
-        DatePicker startDatePicker = new DatePicker();
+
+        startDatePicker = new DatePicker();
         startDatePicker.setEditable(false);
-        DatePicker endDatePicker = new DatePicker();
+        endDatePicker = new DatePicker();
         endDatePicker.setEditable(false);
 
-
-
-        minDate = LocalDate.of(2022, 10, 15);
-        maxDate = LocalDate.of(2023, 2, 9);
         endDatePicker.setDayCellFactory(picker -> new DateCell() {
             @Override
             public void updateItem(LocalDate date, boolean empty) {
                 super.updateItem(date, empty);
                 setDisable(date.isBefore(minDate) || date.isAfter(maxDate));
-
             }
         });
 
@@ -58,77 +89,46 @@ public abstract class BaseGUI {
             public void updateItem(LocalDate date, boolean empty) {
                 super.updateItem(date, empty);
                 setDisable(date.isBefore(minDate) || date.isAfter(maxDate));
-
             }
         });
+
+        Button submitButton = new Button("Submit!");
+        submitButton.setOnAction(event -> submitDate());
 
         startDatePicker.setValue(minDate);
         endDatePicker.setValue(maxDate);
-        menuTab.getChildren().addAll(startDatePicker, endDatePicker);
+        menuTab.getChildren().addAll(startDatePicker, endDatePicker, submitButton);
 
         return menuTab;
     }
-    private HBox buttons() {
-        HBox buttons = new HBox();
-        // Code for buttons
-        Button backButton = new Button(">");
-        Button nextButton = new Button("<");
-        buttons.setSpacing(945);
 
-        nextButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                // Define the action to be performed when next button is clicked
-                //handleDateSelection();
-                System.out.println("Next Button clicked");
-            }
-        });
-
-        backButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                // Define the action to be performed when back button is clicked
-                //handleDateSelection();
-                System.out.println("Back Button clicked");
-            }
-        });
-
-        buttons.getChildren().addAll(nextButton, backButton);
-        return buttons;
-
-
+    public void submitDate(){
+        controller.updateData(selectedDates());
+        controllerGUI.ifAvailable = true;
+        controllerGUI.reloadScene();
     }
 
-    /*private boolean validateDates(LocalDate startDate, LocalDate endDate) {
-        if (startDate == null || endDate == null) {
-            return false;
-        }
-
-        if (startDate.isAfter(endDate) || endDate.isAfter(maxDate) || startDate.isBefore(minDate)) {
-            return false;
-        }
-
-        else {
-            return true;
-        }
-    }
-
-    public void handleDateSelection() {
-        // DateSelected=true;
+    public LocalDate[] selectedDates() {
         startDate = startDatePicker.getValue();
         endDate = endDatePicker.getValue();
-        //Alert alert = new Alert(AlertType.ERROR);
 
         if (!validateDates(startDate, endDate)) {
-            //alert.setTitle("Invalid Dates Selected");
-            //alert.setHeaderText("Please select a valid date range.");
-            //alert.showAndWait();
+
             System.out.println("wrong dates chosen");
 
             startDatePicker.setValue(null);
             endDatePicker.setValue(null);
-            return;
+            return new LocalDate[] {null, null};
+        }
+        return new LocalDate[] {startDate, endDate};
+    }
+
+    private boolean validateDates(LocalDate startDate, LocalDate endDate) {
+        if (startDate == null || endDate == null) {
+            return false;
         }
 
-    }*/
+        return startDate.isBefore(endDate) && endDate.isBefore(maxDate) && startDate.isAfter(minDate);
+    }
+
 }
