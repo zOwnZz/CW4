ccac921350cdd8e2975169d558dbdd77f452bc05
Muhammad-Chikhoun <mchikhoun@outlook.com>
2024-03-13 15:47:26 +0000

    Made map main gui bits
    :dog:

--- ControllerGUI.java
+++ ControllerGUI.java
@@ -1,5 +1,6 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -9,15 +10,19 @@ public class ControllerGUI extends Application {
     public void start(Stage primaryStage) {
         Controller controller = new Controller();
         BaseGUI challenge = new ChallengeGUI(controller);
+        BaseGUI map = new MapGUI(controller);
+
 
         // Get all scenes for the project
-        Scene scene = challenge.getScene();
+        Scene scene = map.getScene();
+        scene.getStylesheets().add("mapStyle.css");
 
         // Set the scene on the stage
         primaryStage.setScene(scene);
 
         // Set the title of the stage (window)
         primaryStage.setTitle("COVID DATA");
+        primaryStage.getIcons().add(new Image("Coronavirus._SARS-CoV-2.png"));
 
         // Show the stage
         primaryStage.show();
