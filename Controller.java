64cafd43f5610db77c1aab5e385b4d9c060186d0
BGlowacki-kcl <152788864+BGlowacki-kcl@users.noreply.github.com>
2024-03-23 23:49:21 +0000

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -4,14 +4,17 @@ import java.util.Comparator;
 import java.util.HashMap;
 
 public class Controller {
-    private ArrayList<CovidData> data;
+    private final ArrayList<CovidData> data;
     private LocalDate startDate, endDate;
+    private final LocalDate minDate, maxDate;
 
     public Controller(){
         CovidDataLoader loader = new CovidDataLoader();
         data = loader.load();
-        startDate = getMinDate();
-        endDate = getMaxDate();
+        minDate = getMinDate();
+        maxDate = getMaxDate();
+        startDate = minDate;
+        endDate = maxDate;
     }
 
     public HashMap<String, ArrayList<CovidData>> boroughAndData() {
@@ -31,11 +34,15 @@ public class Controller {
         return boroughAndData;
     }
 
-    public void updateData(LocalDate[] datesSelected){
-        startDate = datesSelected[0];
-        endDate = datesSelected[1];
+    public void updateData(LocalDate[] datesSelected) {
+        if (startDate != null) {
+            startDate = datesSelected[0];
+            endDate = datesSelected[1];
+        } else {
+            startDate = minDate;
+            endDate = maxDate;
+        }
     }
-
     public LocalDate getMinDate(){
         return data.stream()
                 .map(CovidData::getDate)
@@ -52,16 +59,64 @@ public class Controller {
                 .orElse(null);
     }
 
-    public double calculateAverageCases() {
-        return 0;
+    public LocalDate getStartDate(){
+        return startDate;
+    }
+    public LocalDate getEndDate(){
+        return endDate;
     }
 
-    public int calculateTotalDeaths() {
-        return 0;
+    public double calculateAverageTotalCases(HashMap<String, ArrayList<CovidData>> boroughAndData) {
+        long sumTotalCases = 0;
+        int count = 0;
+        for (ArrayList<CovidData> dataList : boroughAndData.values()) {
+            for (CovidData data : dataList) {
+                sumTotalCases += data.getTotalCases();
+                count++;
+            }
+        }
+        return count > 0 ? Double.parseDouble(String.format("%.3f", (double) sumTotalCases / count)) : 0;
     }
 
-    public double calculateAverageStat(String measure) {
-        return 0;
+    public int calculateTotalDeaths(HashMap<String, ArrayList<CovidData>> boroughAndData) {
+        int sumTotalDeaths = 0;
+        for (ArrayList<CovidData> dataList : boroughAndData.values()) {
+            for (CovidData data : dataList) {
+                sumTotalDeaths += data.getNewDeaths();
+            }
+        }
+        return sumTotalDeaths;
+    }
+
+    public double calculateAverageParksGMR(HashMap<String, ArrayList<CovidData>> boroughAndData) {
+        long sumParksGMR = 0;
+        int count = 0;
+        for (ArrayList<CovidData> dataList : boroughAndData.values()) {
+            for (CovidData data : dataList) {
+                sumParksGMR += data.getParksGMR();
+                count++;
+            }
+        }
+        return count > 0 ? Double.parseDouble(String.format("%.3f", (double) sumParksGMR / count)) : 0;
+    }
+
+    public double calculateAverageTransitGMR(HashMap<String, ArrayList<CovidData>> boroughAndData) {
+        long sumTransitGMR = 0;
+        int count = 0;
+        for (ArrayList<CovidData> dataList : boroughAndData.values()) {
+            for (CovidData data : dataList) {
+                sumTransitGMR += data.getTransitGMR();
+                count++;
+            }
+        }
+        return count > 0 ? Double.parseDouble(String.format("%.3f", (double) sumTransitGMR / count)) : 0;
+    }
+
+    public LocalDate getMinDateCalculated(){
+        return minDate;
+    }
+    public LocalDate getMaxDateCalculated(){
+        return maxDate;
     }
 
 }
