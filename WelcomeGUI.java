ebb131a426c5bd2b14fe0e4b4775052e007a481a
A1SHARIF <116805205+A1SHARIF@users.noreply.github.com>
2024-03-23 20:33:15 +0000

    Update WelcomeGUI.java

--- WelcomeGUI.java
+++ WelcomeGUI.java
@@ -1,40 +1,42 @@
-import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.geometry.Pos;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Region;
+
 public class WelcomeGUI extends BaseGUI {
-     private Controller controller;
-    public WelcomeGUI(Controller controller, HBox buttons){
-        super(controller, buttons);
+    private Controller controller;
+    public WelcomeGUI(Controller controller, ControllerGUI controllerGUI){
+        super(controller, controllerGUI);
         this.controller = controller;
-        
     }
+    public WelcomeGUI(Controller controller, ControllerGUI controllerGUI, Boolean ifFirst){
+        super(controller, controllerGUI, ifFirst);
+        this.controller = controller;
+    }
+
     @Override
     public Scene getScene() {
         BorderPane root = getRoot();
-        
-        VBox center = new VBox();
-    
-        Label InfoLabel = new Label("This application visualises the impact of Covid-19 in London.");
-        
-        // Create a layout to stack the labels on top of each other
-        StackPane stackPane = new StackPane();
-        // Add the labels to the layout
-        stackPane.getChildren().addAll( InfoLabel);
 
-        // Create a scene with the layout
-        Scene scene = new Scene(stackPane, 300, 200);
-        center.getChildren().addAll(InfoLabel);
+        VBox center = new VBox();
+        Label welcomeLabel = new Label("Welcome!");
+        welcomeLabel.setStyle("-fx-font-size: 50px;"); 
+        Label infoLabel = new Label("This application visualises the impact of Covid-19 in London.");
+        Label enjoyLabel = new Label("Enjoy!");
+        Label rulesLabel = new Label("This application visualises the impact of Covid-19 in London.");
+        Label info2Label = new Label("To continue please select the dates which you want to view data from and then click submit.");
+        
         center.setAlignment(Pos.CENTER);
-        center.setSpacing(30);
-        //return new Scene(root, WIN_WIDTH, WIN_HEIGHT);
-        //return scene;
+        
+        VBox.setMargin(welcomeLabel, new Insets(-150, 0, 0, 0));
+        Region gapRegion = new Region();
+        gapRegion.setPrefHeight(100);
+        center.setSpacing(10);
+        center.getChildren().addAll(welcomeLabel,gapRegion, infoLabel,info2Label, enjoyLabel);
         root.setCenter(center);
-        return new Scene(root, WIN_WIDTH, WIN_HEIGHT);
+        return new Scene(root, winWidth, winHeight);
     }
 }
